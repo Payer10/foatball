8bd110cef504b9e5bcd4aa1d4bb192236d082cc4
Payer Ahmed <payerahmedopu@gmail.com>
2026-03-30 18:53:53 +0600

    update code

--- foatball/settings.py
+++ foatball/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-$x-i3t*%ub4oe9j)$5!ycm=9zj6de_kwqtd=ijo5+*361!)%8d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
